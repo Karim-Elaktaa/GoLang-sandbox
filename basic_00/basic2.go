24bd57cadf688da80f6e5c87c32e2be4fed6d98e
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-02-26 23:05:47 +0000

    basic time function : After using select channel

--- basic_00/basic2.go
+++ basic_00/basic2.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 	fmt.Println("Time ", time.Now())
+
+	testAfterWithChannel()
+}
+
+func testAfterWithChannel() {
 	select {
 	case <-time.After(5 * time.Second):
 		fmt.Println("5 seconds")
