766324acd46c94e17ff5e74f7e91b5d367accbe6
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-02-27 22:21:30 +0200

    basic math package : NextAfter

--- basic_math/basic_math.go
+++ basic_math/basic_math.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	rand.Seed(1242)
 	fmt.Println("Random number ", rand.Intn(10))
+	fmt.Println("Next after 2 ", math.Nextafter(2, 3))
 }
