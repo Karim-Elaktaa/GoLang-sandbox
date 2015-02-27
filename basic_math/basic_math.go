35a4d4be4f0691e629d2882cf9aa51dbab59e29d
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-02-27 22:29:33 +0200

    basic math package : Abs, Inf, NaN

--- basic_math/basic_math.go
+++ basic_math/basic_math.go
@@ -9,5 +9,15 @@ import (
 func main() {
 	rand.Seed(1242)
 	fmt.Println("Random number ", rand.Intn(10))
+
 	fmt.Println("Next after 2 ", math.Nextafter(2, 3))
+
+	fmt.Println("PI ", math.Pi)
+	fmt.Println("NextAfter PI ", math.Nextafter(math.Pi, 4))
+
+	fmt.Println("Abs PI", math.Abs(-math.Pi))
+	fmt.Println("Abs NaN ", math.Abs(math.NaN()))
+	fmt.Println("Abs -Inf ", math.Abs(math.Inf(-1)))
+	fmt.Println("Abs +Inf ", math.Abs(math.Inf(1)))
+
 }
