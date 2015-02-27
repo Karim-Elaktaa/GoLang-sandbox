073c13e7cc20085bf01b8ada34e1fbb459b97d19
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-02-27 23:03:24 +0200

    basic math package : Bessel function

--- basic_math/basic_math.go
+++ basic_math/basic_math.go
@@ -20,4 +20,14 @@ func main() {
 	fmt.Println("Abs -Inf ", math.Abs(math.Inf(-1)))
 	fmt.Println("Abs +Inf ", math.Abs(math.Inf(1)))
 
+	fmt.Println("Ceil -PI ", math.Ceil(-math.Pi))
+
+	fmt.Println("Copysign ", math.Copysign(4, -1))
+
+	fmt.Println("Dim ", math.Dim(4, -1))
+
+	fmt.Println("PI Bessel order zero ", math.J0(math.Pi))
+	fmt.Println("PI Bessel order one ", math.J1(math.Pi))
+	fmt.Println("3.14 Bessel order 13 ", math.Jn(3.14, 13))
+
 }
